9c9d2eb46da9fc68e0a420cf80f67c3939f49bee
Jasper <57488312+Jasper-27@users.noreply.github.com>
2021-04-14 18:41:44 +0100

    moved the comment in the go file
    This caused the code to be reformatted in the "correct" golang way. So i suppose that is better 🙄

--- Code/FizzBuzz.go
+++ Code/FizzBuzz.go
@@ -1,24 +1,27 @@
-// FizzBuzz in Golang  https://github.com/Jasper-27
-
 package main
+
+// FizzBuzz in Golang  https://github.com/Jasper-27
 
 import "fmt"
 
 var fizz int = 3
 var buzz int = 5
 
-
 func main() {
 
 	for i := 1; i < 100; i++ {
 		var out = ""
 
-		if i % fizz == 0 { out += "Fizz" }
-		if i % buzz == 0 { out += "Buzz" }
+		if i%fizz == 0 {
+			out += "Fizz"
+		}
+		if i%buzz == 0 {
+			out += "Buzz"
+		}
 
-		if out == ""{
+		if out == "" {
 			fmt.Println(i)
-		}else {
+		} else {
 			fmt.Println(out)
 		}
 	}
